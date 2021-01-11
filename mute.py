6e01e429d674d736f760bd3624a1ca17d88008c1
Natanlimap <natanlimap@gmail.com>
2021-01-11 11:17:24 -0300

    Comando flanas feito

--- mute.py
+++ mute.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-
 import json
 import os
+import ddmais as dd
 
 
 # verificar se o arquivo de configuração existe
@@ -10,6 +10,9 @@ if os.path.exists(os.getcwd() + "/config.json"):
     
     with open("./config.json") as f:
         configData = json.load(f)
+    
+    with open("./ddmais.json") as ddmaisJson:
+        configDataDDMais = json.load(ddmaisJson)
 
 else:
     # criando um arquivo de configuração padrão
@@ -21,13 +24,18 @@ else:
 # passando os parametros do arquivo de configuração
 token = configData["Token"]
 prefix = configData["Prefix"]
+flanasCounter = configDataDDMais["Flanas"]
 
 bot = commands.Bot(command_prefix=prefix)
 
 @bot.event
-async def on_ready():
+async def on_ready():    
     print("Bot is ready.")
 
-
+@bot.command()
+async def flanas(ctx):
+    global flanasCounter
+    flanasCounter = flanasCounter + 1
+    await dd.flanas(ctx, flanasCounter)
 
 bot.run(token)
